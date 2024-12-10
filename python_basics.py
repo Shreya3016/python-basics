d373ff4bdec56014a8b9333888a1bf330d594faa
Shreyashri AC <shreyashri3031999@gmail.com>
2024-12-10 12:03:01 +0530

    basics add

--- python_basics.py
+++ python_basics.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from collections import Counter
 
 def sort_by_frequency(s):
@@ -17,46 +16,3 @@ def sort_by_frequency(s):
 s = "tree"
 sorted_s = sort_by_frequency(s)
 print(sorted_s)  # Output: "eert"
-=======
-public class StudentRank {
-    private String[] students;
-    private int[] ranks;
-
-    // Constructor
-    public StudentRank(String[] students, int[] ranks) {
-        this.students = students;
-        this.ranks = ranks;
-    }
-
-    // Method to get the highest-ranked student
-    public String highestRank() {
-        int maxRankIndex = 0;
-        for (int i = 1; i < ranks.length; i++) {
-            if (ranks[i] > ranks[maxRankIndex]) {
-                maxRankIndex = i;
-            }
-        }
-        return students[maxRankIndex];
-    }
-
-    // Method to get the lowest-ranked student
-    public String lowestRank() {
-        int minRankIndex = 0;
-        for (int i = 1; i < ranks.length; i++) {
-            if (ranks[i] < ranks[minRankIndex]) {
-                minRankIndex = i;
-            }
-        }
-        return students[minRankIndex];
-    }
-
-    public static void main(String[] args) {
-        String[] students = {"Taylor", "Wesley", "Jordan"};
-        int[] ranks = {1, 5, 3};
-        StudentRank sr = new StudentRank(students, ranks);
-
-        System.out.println(sr.highestRank()); // Output: Wesley
-        System.out.println(sr.lowestRank());  // Output: Taylor
-    }
-}
->>>>>>> 1bfcaa02198f92b3aa0940982549dd2c61f9695e
