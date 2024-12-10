48dc35dc3e87581e3368d581f47e547298accf01
Shreya3016 <shreyashri3031999@gmail.com>
2024-12-10 12:34:14 +0530

    Update python_basics.py

--- python_basics.py
+++ python_basics.py
@@ -1,18 +1,35 @@
-from collections import Counter
+from flask import request, jsonify
+from . import events
+from .models import Event
++import uuid
++
++@events.route('/<int:id>', methods=['DELETE'])
++def delete_event(id):
++    event = Event.get_event(id)
++    if event:
++        Event.events.remove(event)
++        return jsonify({'message': 'Event deleted'})
++    return ('Event not found', 404)
 
-def sort_by_frequency(s):
-    # Count the frequency of each character
-    freq = Counter(s)
-    
-    # Sort characters by frequency and then alphabetically
-    sorted_chars = sorted(freq.items(), key=lambda x: (-x[1], x[0]))
-    
-    # Build the sorted string
-    sorted_string = ''.join(char * count for char, count in sorted_chars)
-    
-    return sorted_string
+@events.route('/', methods=['GET'])
+def get_events():
+    events = Event.get_all_events()
+    return jsonify({'events': [event.to_dict() for event in events]})
+@events.route('/<int:id>', methods=['GET'])
+def get_event(id):
+    event = Event.get_event(id)
+    return jsonify(event.to_dict()) if event else ('Event not found')
 
-# Example usage:
-s = "tree"
-sorted_s = sort_by_frequency(s)
-print(sorted_s)  # Output: "eert"
+- @events.route('/', methods=['PST'])
++ @events.route('/', methods=['POST'])
+def add_event():
+-   id = len(Event.events) + 1
++    id = str(uuid.uuid4()) if Event.events else 1
++    if not request.json or not 'name' in request.json or not 'location' in request.json:
++        return jsonify({'error': 'Missing mandatory fields: name and/or location'}), 400
+    name = request.json.get('name')
+    location = request.json.get('location')
+    event = Event(id, name, location)
+-    event.add_event()
++    Event.add_event(event)
+    return jsonify({'message': 'Event added', 'event': event.to_dict()}), 201
